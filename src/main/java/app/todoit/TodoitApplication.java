e1ac6a615a49a065e8bbbcaf48240fa272ff5edb
GyeongEun-Kim <kekim20@naver.com>
2023-02-17 16:14:54 +0900

    first setting:

---
+++ src/main/java/app/todoit/TodoitApplication.java
@@ -0,0 +1,13 @@
+package app.todoit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/todoit/TodoitApplicationTests.java
@@ -0,0 +1,13 @@
+package app.todoit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
