63848cfac13b98b75890ef052b94e6fe96ad3ca0
NamYeju <dpwn0112@naver.com>
2023-03-16 23:59:05 +0900

    feat: 예외처리 추가

---
+++ src/main/java/app/todoit/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package app.todoit.global.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import app.todoit.auth.exception.MemberException;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+	@ExceptionHandler
+	public ResponseEntity<ErrorResponse> memberException(MemberException e){
+		log.info("member exception >>>", e);
+
+		return ErrorResponse.toResponse(e.getErrorCode());
+	}
+}
