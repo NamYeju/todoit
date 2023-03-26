0b1d7e58971ecfa3dd4b8556b57be9ebfdd4820e
GyeongEun-Kim <kekim20@naver.com>
2023-03-27 00:08:19 +0900

    쓰레드 로컬에서 유저정보 가져오기/테스트코드/예외 구현

--- src/main/java/app/todoit/domain/friend/api/FriendController.java
+++ src/main/java/app/todoit/domain/friend/api/FriendController.java
@@ -1,10 +1,12 @@
 package app.todoit.domain.friend.api;
 
+import app.todoit.domain.auth.entity.User;
 import app.todoit.domain.friend.dto.FriendResponseDto;
 import app.todoit.domain.friend.dto.PendingFriendResponseDto;
 import app.todoit.domain.friend.entity.FriendEntity;
 import app.todoit.domain.friend.entity.PendingFriendEntity;
 import app.todoit.domain.friend.service.FriendService;
+import app.todoit.global.interceptor.UserThreadLocal;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,27 +18,31 @@ public class FriendController {
     private final FriendService friendService;
     @PostMapping("/friend")
     public String addFriend (@RequestParam Long friendId) { //친구 신청
-        return friendService.addFriend(3L,friendId);
+        User user = UserThreadLocal.get();
+        return friendService.addFriend(user,friendId);
     }
 
     @GetMapping("/friend")
     public FriendResponseDto getFriendList() {
-        return friendService.getFriendsList(1L);
+        User user = UserThreadLocal.get();
+        return friendService.getFriendsList(user);
     }
 
     @DeleteMapping("/friend")
     public String deleteFriend(@RequestParam Long friendId) { //친구 삭제
-
-        return friendService.deleteFriend(1L,friendId);
+        User user = UserThreadLocal.get();
+        return friendService.deleteFriend(user,friendId);
     }
 
     @GetMapping("/friend/pending")
     public PendingFriendResponseDto getPendingFriendList() { //신청 대기 목록 조회
-        return friendService.getPendingFriends(2L);
+        User user = UserThreadLocal.get();
+        return friendService.getPendingFriends(user);
     }
 
     @PostMapping("/friend/accept")
     public String acceptFriend(@RequestParam Long friendId) { //친구 수락
-        return friendService.acceptFriend(1L,friendId);
+        User user = UserThreadLocal.get();
+        return friendService.acceptFriend(user,friendId);
     }
 }
