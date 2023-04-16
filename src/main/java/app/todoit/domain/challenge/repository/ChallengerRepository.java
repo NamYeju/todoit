00ada2b6efa214649dcfc648eaf4e27dc81fd094
NamYeju <dpwn0112@naver.com>
2023-04-16 22:50:13 +0900

    feat: 클라스명 수정

--- src/main/java/app/todoit/domain/challenge/repository/ChallengerRepository.java
+++ src/main/java/app/todoit/domain/challenge/repository/ChallengerRepository.java
@@ -3,8 +3,8 @@ package app.todoit.domain.challenge.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import app.todoit.domain.challenge.entity.ChallengeMember;
+import app.todoit.domain.challenge.entity.Challenger;
 
 @Repository
-public interface ChallengerRepository extends JpaRepository<ChallengeMember, Long> {
+public interface ChallengerRepository extends JpaRepository<Challenger, Long> {
 }
