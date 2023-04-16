8ae88e2542404064bcfd3e944cbcc1451fcd58e1
NamYeju <dpwn0112@naver.com>
2023-04-16 22:49:36 +0900

    feat: dto 날짜 포맷 추가

--- src/main/java/app/todoit/domain/challenge/dto/ChallengeDto.java
+++ src/main/java/app/todoit/domain/challenge/dto/ChallengeDto.java
@@ -1,6 +1,9 @@
 package app.todoit.domain.challenge.dto;
 
+import java.util.Date;
 import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -12,10 +15,15 @@ public class ChallengeDto {
 	@AllArgsConstructor
 	public static class Request {
 		private String title;
+
 		private String[] day;
 		private String[] off_day;
-		private String start_date;
-		private String end_date;
+
+		@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-mm-dd")
+		private Date start_date;
+		@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-mm-dd")
+		private Date end_date;
+
 		private List<Challenger> friends;
 		private Challenger inviter;
 
